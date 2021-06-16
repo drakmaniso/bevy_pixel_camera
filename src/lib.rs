86d782855e21c45b5faa05e4a6231e9b80cabd48
drakmaniso <moussault.laurent@gmail.com>
2021-06-16 16:47:00 +0200

    Improve doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,33 @@
-// Pixel-Art Plugin
+//! A simple pixel-perfect camera plugin for Bevy, suitable for pixel-art.
+//!
+//! ```no_compile
+//! use bevy::prelude::*;
+//! use bevy_pixel_camera::
+//!
+//! fn main() {
+//!     App::build()
+//!         .add_plugins(DefaultPlugins)
+//!         .add_plugin(PixelCameraPlugin)
+//!         .add_startup_system(setup.system())
+//!         .run();
+//! }
+//!
+//! fn setup(
+//!     mut commands: Commands,
+//!     asset_server: Res<AssetServer>,
+//!     mut materials: ResMut<Assets<ColorMaterial>>,
+//!     quad: Res<PixelSpriteQuad>,
+//! ) {
+//!     commands.spawn_bundle(PixelCameraBundle::from_zoom(3));
+//!
+//!     let sprite_handle = materials.add(asset_server.load("my-pixel-art-sprite.png").into());
+//!     commands.spawn_bundle(SpriteBundle {
+//!         material: sprite_handle.clone(),
+//!         mesh: quad.clone().into(),
+//!         ..Default::default()
+//!     });
+//! }
+//! ```
 
 use bevy::prelude::{
     AppBuilder, Assets, Bundle, Commands, CoreStage, GlobalTransform, Handle, IntoSystem, Mat4,

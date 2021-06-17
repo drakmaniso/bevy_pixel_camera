2f6cab8c0a735d1d52418e5c8090a7d2b17cb3c0
drakmaniso <moussault.laurent@gmail.com>
2021-06-17 11:49:39 +0200

    Improve docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,24 @@
 //! A simple pixel-perfect camera plugin for Bevy, suitable for pixel-art.
 //!
+//! This crates makes it possible to correctly display low-resolution 2d
+//! graphics with Bevy's default renderer and sprites.
+//!
+//! While it is possible to achieve pixel-perfect rendering of such sprites with
+//! Bevy's own camera and `OrthographicProjection`, doing so correctly requires
+//! to configure in a specific way (you can't just use
+//! `OrhtographicCameraBundle`).
+//!
+//! This plugin offers a camera which can be configured by specifying either the
+//! size of the virtual pixels, or the desired resolution.
+//!
+//! It also includes a quad mesh resource to replace the default one used in
+//! Bevy's `SpriteBundle`. The default quad has its origin at the center of the
+//! image, but if the image has an odd width or height, that origin is not
+//! pixel-aligned. The resource included in this plugin puts the origin at the
+//! bottom-left corner of the image.
+//!
+//! ## Example code
+//!
 //! ```no_compile
 //! use bevy::prelude::*;
 //! use bevy_pixel_camera::

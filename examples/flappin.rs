0f3b68979bcc9565e12a40012e448710d87b014a
drakmaniso <moussault.laurent@gmail.com>
2021-06-22 09:11:05 +0200

    Cleanup

--- examples/flappin.rs
+++ examples/flappin.rs
@@ -5,6 +5,8 @@ use bevy::{
 };
 use bevy_pixel_camera::{PixelCameraBundle, PixelCameraPlugin, PixelSpriteQuad};
 
+// GAME CONSTANTS /////////////////////////////////////////////////////////////
+
 const WIDTH: f32 = 240.0;
 const HEIGHT: f32 = 240.0;
 const LEFT: f32 = -WIDTH / 2.0;
@@ -30,22 +32,15 @@ const FRAME: f64 = 1.0 / 60.0;
 
 const FALLING_JERK: f32 = -2300.0;
 const FLAP_VELOCITY: f32 = 100.0;
-const FLAP_ACCELERATION: f32 = 80.0;
+const FLAP_ACCELERATION: f32 = 90.0;
+
+// SETUP //////////////////////////////////////////////////////////////////////
 
 #[derive(Clone, Eq, PartialEq, Hash, Debug)]
 enum GameState {
     StartScreen,
     Playing,
     GameOver,
-}
-
-struct Bird;
-struct Cloud;
-struct Pillar;
-
-struct BirdPhysics {
-    velocity: f32,
-    acceleration: f32,
 }
 
 fn main() {
@@ -64,6 +59,7 @@ fn main() {
         .insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)))
         .insert_resource(Timer::from_seconds(0.25, false))
         .add_startup_system(setup.system().label("setup"))
+        .add_startup_system(spawn_bird.system().after("setup"))
         .add_startup_system(spawn_clouds.system().after("setup"))
         .add_system(bevy::input::system::exit_on_esc_system.system())
         .add_system_set(
@@ -98,14 +94,7 @@ fn main() {
         .run();
 }
 
-fn setup(
-    mut commands: Commands,
-    time: Res<Time>,
-    mut rng: ResMut<Rng>,
-    asset_server: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-    quad: Res<PixelSpriteQuad>,
-) {
+fn setup(mut commands: Commands, time: Res<Time>, mut rng: ResMut<Rng>) {
     *rng = Rng {
         mz: time.startup().elapsed().as_secs() as u32,
         mw: 678,
@@ -115,7 +104,51 @@ fn setup(
         WIDTH as i32,
         HEIGHT as i32,
     ));
+}
 
+// START SCREEN ///////////////////////////////////////////////////////////////
+
+fn press_to_start(
+    mut key_events: EventReader<KeyboardInput>,
+    mut state: ResMut<State<GameState>>,
+    time: Res<Time>,
+    mut timer: ResMut<Timer>,
+    mut birds: Query<(&mut Transform, &mut BirdPhysics), With<Bird>>,
+) {
+    timer.tick(time.delta());
+    if timer.finished()
+        && key_events
+            .iter()
+            .any(|ev| ev.state == ElementState::Pressed)
+    {
+        for (mut transform, mut physics) in birds.iter_mut() {
+            *transform = Transform::from_xyz(BIRD_X, 0.0, 1.0);
+            physics.velocity = FLAP_VELOCITY;
+            physics.acceleration = FLAP_ACCELERATION;
+        }
+        state
+            .set(GameState::Playing)
+            .expect("Problem while switching to playing state");
+    }
+}
+
+// THE BIRD ///////////////////////////////////////////////////////////////////
+
+// Component
+struct Bird;
+
+// Component
+struct BirdPhysics {
+    velocity: f32,
+    acceleration: f32,
+}
+
+fn spawn_bird(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    quad: Res<PixelSpriteQuad>,
+) {
     let texture_atlas = texture_atlases.add(TextureAtlas::from_grid(
         asset_server.load("flappin-bird.png"),
         Vec2::new(26.0, 21.0),
@@ -136,70 +169,6 @@ fn setup(
             ..Default::default()
         })
         .insert(Timer::from_seconds(0.150, true));
-}
-
-fn spawn_clouds(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-    quad: Res<PixelSpriteQuad>,
-    mut rng: ResMut<Rng>,
-) {
-    let clouds_atlas = texture_atlases.add(TextureAtlas::from_grid(
-        asset_server.load("flappin-clouds.png"),
-        Vec2::new(CLOUD_WIDTH, CLOUD_HEIGHT),
-        4,
-        1,
-    ));
-
-    let mut x = LEFT;
-    while x < RIGHT {
-        let y = BOTTOM + 40.0 + rng.rand_range(0..(HEIGHT - 80.0 - CLOUD_HEIGHT) as u32) as f32;
-        commands
-            .spawn()
-            .insert(Cloud)
-            .insert_bundle(SpriteSheetBundle {
-                texture_atlas: clouds_atlas.clone(),
-                transform: Transform::from_xyz(x, y, 0.0),
-                sprite: TextureAtlasSprite {
-                    index: rng.rand_range(0..4),
-                    ..Default::default()
-                },
-                mesh: quad.clone().into(),
-                ..Default::default()
-            });
-        x += CLOUD_WIDTH;
-    }
-}
-
-fn spawn_pillars(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-    quad: Res<PixelSpriteQuad>,
-    mut rng: ResMut<Rng>,
-) {
-    let atlas = texture_atlases.add(TextureAtlas::from_grid(
-        asset_server.load("flappin-pillars.png"),
-        Vec2::new(PILLAR_WIDTH, PILLAR_HEIGHT),
-        1,
-        1,
-    ));
-
-    let mut x = RIGHT;
-    while x < RIGHT + WIDTH + PILLAR_SPACING {
-        let y = (rng.rand_range(0..PILLAR_RANGE as u32) as f32 - PILLAR_RANGE / 2.0).round();
-        commands
-            .spawn()
-            .insert(Pillar)
-            .insert_bundle(SpriteSheetBundle {
-                texture_atlas: atlas.clone(),
-                transform: Transform::from_xyz(x, (y - PILLAR_HEIGHT / 2.0).round(), 2.0),
-                mesh: quad.clone().into(),
-                ..Default::default()
-            });
-        x += PILLAR_SPACING;
-    }
 }
 
 fn animate_flying_bird(
@@ -234,42 +203,6 @@ fn animate_flappin_bird(
     }
 }
 
-fn animate_pillars(
-    mut rng: ResMut<Rng>,
-    state: Res<State<GameState>>,
-    mut query: Query<&mut Transform, With<Pillar>>,
-) {
-    if *state.current() == GameState::GameOver {
-        return;
-    }
-    for mut transform in query.iter_mut() {
-        *transform = transform.mul_transform(Transform::from_xyz(-1.0, 0.0, 0.0));
-        if transform.translation.x + PILLAR_SPACING < LEFT {
-            let y = (rng.rand_range(0..PILLAR_RANGE as u32) as f32 - PILLAR_RANGE / 2.0).round();
-            *transform = Transform::from_xyz(RIGHT, (y - PILLAR_HEIGHT / 2.0).round(), 2.0);
-        }
-    }
-}
-
-fn animate_clouds(
-    mut rng: ResMut<Rng>,
-    state: Res<State<GameState>>,
-    mut query: Query<(&mut Transform, &mut TextureAtlasSprite), With<Cloud>>,
-) {
-    if *state.current() == GameState::GameOver {
-        return;
-    }
-    for (mut transform, mut sprite) in query.iter_mut() {
-        *transform = transform.mul_transform(Transform::from_xyz(-1.0, 0.0, 0.0));
-        if transform.translation.x + CLOUD_WIDTH < LEFT {
-            let y = BOTTOM + 40.0 + rng.rand_range(0..(HEIGHT - 80.0 - CLOUD_HEIGHT) as u32) as f32;
-            *transform = Transform::from_xyz(RIGHT, y, 0.0);
-            sprite.index = rng.rand_range(0..4);
-            sprite.flip_x = rng.rand_range(0..2) > 0;
-        }
-    }
-}
-
 fn flap(
     mut key_events: EventReader<KeyboardInput>,
     mut birds: Query<&mut BirdPhysics, With<Bird>>,
@@ -282,6 +215,13 @@ fn flap(
             physics.velocity = FLAP_VELOCITY;
             physics.acceleration = FLAP_ACCELERATION;
         }
+    }
+}
+
+fn game_over(mut timer: ResMut<Timer>, mut birds: Query<&mut TextureAtlasSprite, With<Bird>>) {
+    timer.reset();
+    for mut sprite in birds.iter_mut() {
+        sprite.index = 3;
     }
 }
 
@@ -322,6 +262,124 @@ fn collision_detection(
     }
 }
 
+// THE PILLARS ////////////////////////////////////////////////////////////////
+
+// Component
+struct Pillar;
+
+fn spawn_pillars(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    quad: Res<PixelSpriteQuad>,
+    mut rng: ResMut<Rng>,
+) {
+    let atlas = texture_atlases.add(TextureAtlas::from_grid(
+        asset_server.load("flappin-pillars.png"),
+        Vec2::new(PILLAR_WIDTH, PILLAR_HEIGHT),
+        1,
+        1,
+    ));
+
+    let mut x = RIGHT;
+    while x < RIGHT + WIDTH + PILLAR_SPACING {
+        let y = (rng.rand_range(0..PILLAR_RANGE as u32) as f32 - PILLAR_RANGE / 2.0).round();
+        commands
+            .spawn()
+            .insert(Pillar)
+            .insert_bundle(SpriteSheetBundle {
+                texture_atlas: atlas.clone(),
+                transform: Transform::from_xyz(x, (y - PILLAR_HEIGHT / 2.0).round(), 2.0),
+                mesh: quad.clone().into(),
+                ..Default::default()
+            });
+        x += PILLAR_SPACING;
+    }
+}
+
+fn animate_pillars(
+    mut rng: ResMut<Rng>,
+    state: Res<State<GameState>>,
+    mut query: Query<&mut Transform, With<Pillar>>,
+) {
+    if *state.current() == GameState::GameOver {
+        return;
+    }
+    for mut transform in query.iter_mut() {
+        *transform = transform.mul_transform(Transform::from_xyz(-1.0, 0.0, 0.0));
+        if transform.translation.x + PILLAR_SPACING < LEFT {
+            let y = (rng.rand_range(0..PILLAR_RANGE as u32) as f32 - PILLAR_RANGE / 2.0).round();
+            *transform = Transform::from_xyz(RIGHT, (y - PILLAR_HEIGHT / 2.0).round(), 2.0);
+        }
+    }
+}
+
+fn despawn_pillars(mut commands: Commands, pillars: Query<Entity, With<Pillar>>) {
+    for id in pillars.iter() {
+        commands.entity(id).despawn();
+    }
+}
+
+// THE CLOUDS /////////////////////////////////////////////////////////////////
+
+// Component
+struct Cloud;
+
+fn spawn_clouds(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    quad: Res<PixelSpriteQuad>,
+    mut rng: ResMut<Rng>,
+) {
+    let clouds_atlas = texture_atlases.add(TextureAtlas::from_grid(
+        asset_server.load("flappin-clouds.png"),
+        Vec2::new(CLOUD_WIDTH, CLOUD_HEIGHT),
+        4,
+        1,
+    ));
+
+    let mut x = LEFT;
+    while x < RIGHT {
+        let y = BOTTOM + 40.0 + rng.rand_range(0..(HEIGHT - 80.0 - CLOUD_HEIGHT) as u32) as f32;
+        commands
+            .spawn()
+            .insert(Cloud)
+            .insert_bundle(SpriteSheetBundle {
+                texture_atlas: clouds_atlas.clone(),
+                transform: Transform::from_xyz(x, y, 0.0),
+                sprite: TextureAtlasSprite {
+                    index: rng.rand_range(0..4),
+                    ..Default::default()
+                },
+                mesh: quad.clone().into(),
+                ..Default::default()
+            });
+        x += CLOUD_WIDTH;
+    }
+}
+
+fn animate_clouds(
+    mut rng: ResMut<Rng>,
+    state: Res<State<GameState>>,
+    mut query: Query<(&mut Transform, &mut TextureAtlasSprite), With<Cloud>>,
+) {
+    if *state.current() == GameState::GameOver {
+        return;
+    }
+    for (mut transform, mut sprite) in query.iter_mut() {
+        *transform = transform.mul_transform(Transform::from_xyz(-1.0, 0.0, 0.0));
+        if transform.translation.x + CLOUD_WIDTH < LEFT {
+            let y = BOTTOM + 40.0 + rng.rand_range(0..(HEIGHT - 80.0 - CLOUD_HEIGHT) as u32) as f32;
+            *transform = Transform::from_xyz(RIGHT, y, 0.0);
+            sprite.index = rng.rand_range(0..4);
+            sprite.flip_x = rng.rand_range(0..2) > 0;
+        }
+    }
+}
+
+// UTILITIES //////////////////////////////////////////////////////////////////
+
 fn circle_box_collide(
     cx: f32,
     cy: f32,
@@ -342,44 +400,7 @@ fn clamp(v: f32, lower: f32, upper: f32) -> f32 {
     lower.max(upper.min(v))
 }
 
-fn press_to_start(
-    mut key_events: EventReader<KeyboardInput>,
-    mut state: ResMut<State<GameState>>,
-    time: Res<Time>,
-    mut timer: ResMut<Timer>,
-    mut birds: Query<(&mut Transform, &mut BirdPhysics), With<Bird>>,
-) {
-    timer.tick(time.delta());
-    if timer.finished()
-        && key_events
-            .iter()
-            .any(|ev| ev.state == ElementState::Pressed)
-    {
-        for (mut transform, mut physics) in birds.iter_mut() {
-            *transform = Transform::from_xyz(BIRD_X, 0.0, 1.0);
-            physics.velocity = FLAP_VELOCITY;
-            physics.acceleration = FLAP_ACCELERATION;
-        }
-        state
-            .set(GameState::Playing)
-            .expect("Problem while switching to playing state")
-    }
-}
-
-fn game_over(mut timer: ResMut<Timer>, mut birds: Query<&mut TextureAtlasSprite, With<Bird>>) {
-    timer.reset();
-    for mut sprite in birds.iter_mut() {
-        sprite.index = 3;
-    }
-}
-
-fn despawn_pillars(mut commands: Commands, pillars: Query<Entity, With<Pillar>>) {
-    for id in pillars.iter() {
-        commands.entity(id).despawn();
-    }
-}
-
-// RNG
+// RNG ////////////////////////////////////////////////////////////////////////
 
 struct Rng {
     mz: u32,

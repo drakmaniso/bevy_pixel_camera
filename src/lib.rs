e5f8c4a4f21abe22775d781a117f72dd1efa951f
drakmaniso <moussault.laurent@gmail.com>
2021-06-24 12:10:37 +0200

    Swith to dual license
    For maximum compatibility with Bevy.

--- src/lib.rs
+++ src/lib.rs
@@ -75,6 +75,15 @@
 //!     });
 //! }
 //! ```
+//!
+//! ## License
+//!
+//! Licensed under either of
+//!
+//! * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+//! * MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+//!
+//! at your option.
 
 mod pixel_border;
 mod pixel_camera;

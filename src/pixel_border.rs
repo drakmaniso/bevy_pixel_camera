ef7c33ce65bd3821eb37853c71f2030445e8a65c
Arne Winter <arnemileswinter@gmail.com>
2022-11-10 11:09:45 +0100

    fixed assumption of Transform and GlobalTransform sharing a common interface.

--- src/pixel_border.rs
+++ src/pixel_border.rs
@@ -64,20 +64,20 @@ fn resize_borders(
         (&PixelProjection, &GlobalTransform),
         Or<(Changed<PixelProjection>, Changed<GlobalTransform>)>,
     >,
-    mut borders: Query<(&mut Sprite, &mut GlobalTransform, &Border), Without<PixelProjection>>,
+    mut borders: Query<(&mut Sprite, &mut Transform, &Border), Without<PixelProjection>>,
 ) {
     if let Some((projection, transform)) = cameras.iter().next() {
         let z = projection.far - 0.2;
         let width = projection.desired_width.map(|w| w as f32).unwrap_or(0.0);
         let height = projection.desired_height.map(|h| h as f32).unwrap_or(0.0);
-        let left = transform.translation.x
+        let left = transform.translation().x
             + if projection.centered {
                 -(width / 2.0).round()
             } else {
                 0.0
             };
         let right = left + width;
-        let bottom = transform.translation.y
+        let bottom = transform.translation().y
             + if projection.centered {
                 (-height / 2.0).round()
             } else {
@@ -88,19 +88,19 @@ fn resize_borders(
         for (mut sprite, mut transform, border) in borders.iter_mut() {
             match border {
                 Border::Left => {
-                    *transform = GlobalTransform::from_xyz(left - width, bottom - height, z);
+                    *transform = Transform::from_xyz(left - width, bottom - height, z);
                     sprite.custom_size = Some(Vec2::new(width, 3.0 * height));
                 }
                 Border::Right => {
-                    *transform = GlobalTransform::from_xyz(right, bottom - height, z);
+                    *transform = Transform::from_xyz(right, bottom - height, z);
                     sprite.custom_size = Some(Vec2::new(width, 3.0 * height));
                 }
                 Border::Top => {
-                    *transform = GlobalTransform::from_xyz(left - width, top, z);
+                    *transform = Transform::from_xyz(left - width, top, z);
                     sprite.custom_size = Some(Vec2::new(3.0 * width, height));
                 }
                 Border::Bottom => {
-                    *transform = GlobalTransform::from_xyz(left - width, bottom - height, z);
+                    *transform = Transform::from_xyz(left - width, bottom - height, z);
                     sprite.custom_size = Some(Vec2::new(3.0 * width, height));
                 }
             }
